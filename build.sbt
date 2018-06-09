a8415cdff38a217915b4baa4fd862adf0da7acd5
Anton Potemkin <79133891911@yandex.ru>
2018-06-09 12:08:01 +0700

    update parser

--- build.sbt
+++ build.sbt
@@ -6,3 +6,6 @@ scalaVersion := "2.12.1"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.4" % "test"
 libraryDependencies += "junit" % "junit" % "4.10" % Test
+
+//for parser
+libraryDependencies += "org.codehaus.jackson" % "jackson-core-asl" % "1.6.1"
--- src/test/scala/anton/potemkin/scala_school/com/twitter/sample/TwitterParserTest.scala
+++ src/test/scala/anton/potemkin/scala_school/com/twitter/sample/TwitterParserTest.scala
@@ -8,8 +8,47 @@ import org.scalatest.junit.JUnitRunner
 class TwitterParserTest extends FunSuite {
 
   val tweet = """{"id":1,"text":"foo"}"""
+  val parser = new SimpleParser()
+
   test("first test") {
-    val expected = Some(SimpleParsed(1,"foo"))
-    assert(new SimpleParser().parse(tweet) == expected)
+    val expected = Some(SimpleParsed(1, "foo"))
+    assert(parser.parse(tweet) == expected)
+
+    //    val expected = SimpleParsed(1,"foo")
+    //    new SimpleParser().parse(tweet) match {
+    //      case Some(parsed) => {
+    //        assert(parsed.text == expected.text)
+    //        assert(parsed.id  == expected.id)
+    //      }
+    //      case _ => fail("didn't parse tweet")
+    //    }
+  }
+
+  test("ignore nested content") {
+    val tweet = """{"id":1,"text":"foo","nested":{"id":2}}"""
+    val expected = SimpleParsed(1, "foo")
+    parser.parse(tweet) match {
+      case Some(parsed) => {
+        assert(parsed.text == expected.text)
+        assert(parsed.id == expected.id)
+      }
+      case _ => fail("didn't parse tweet")
+    }
+  }
+
+  test("reject a non-JSON tweet") {
+    val tweet = """"id":1"text":"foo""""
+    parser.parse(tweet) match {
+      case Some(parsed) => fail("didn't reject a non-JSON tweet")
+      case e => assert(e.isEmpty)
+    }
+  }
+
+  test("fail on partial content") {
+    val tweet = """{"id":1}"""
+    parser.parse(tweet) match {
+      case Some(parsed) => fail("didn't reject a partial tweet")
+      case e => assert(e.isEmpty)
+    }
   }
 }
